61867b73f97ffa129ee5ab3a9bdbcd9c7fcac246
hritik-hk <kumar.hritik4w@gmail.com>
2024-03-10 02:04:28 +0530

    added posts schema, create posts query, saving scraper fetched feed to db

--- scraper.go
+++ scraper.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/xml"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/hritik-hk/rss-aggregator/internal/database"
 )
 
@@ -88,7 +91,38 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 		return
 	}
 	for _, item := range feedData.Channel.Item {
-		log.Println("Found post", item.Title)
+
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, er := time.Parse(time.RFC1123Z, item.PubDate)
+		if er != nil {
+			log.Printf("couldn't parse publishAt date %v , err: %v", item.PubDate, er)
+			continue
+		}
+
+		_, err := db.CreatePost(
+			context.Background(),
+			database.CreatePostParams{
+				ID:          uuid.New(),
+				CreatedAt:   time.Now().UTC(),
+				UpdatedAt:   time.Now().UTC(),
+				Title:       item.Title,
+				Description: description,
+				PublishedAt: pubAt,
+				Url:         item.Link,
+				FeedID:      feed.ID,
+			})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("failed to create post: ", err)
+		}
+
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
 }
